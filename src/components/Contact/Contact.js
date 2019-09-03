717757de483961483739362b7b239adaa76b8de8
Gerardo Paredes <gerardparedes23@gmail.com>
2019-09-02 17:24:34 -0700

    contact component added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,13 +11,19 @@ const contact = () => (
     <div className="contact">
         <div className="contact__container">
             <h2 className="contact__title">FRONT END DEVELOPER</h2>
-            <img src={codeIcon} alt="" className="contact__img" />
+            <img src={codeIcon} alt="code icon" className="contact__img" />
             <p className="contact__text">GERARDO PAREDES</p>
             <p className="contact__email">gerardparedes23@gmail.com</p>
             <div className="contact__icon--wrap">
-                <img src={gitIcon} alt="" className="contact__icon" />
-                <img src={emailIcon} alt="" className="contact__icon" />
-                <img src={linkedin} alt="" className="contact__icon" />
+                <a href="https://github.com/gerardopar" target="noopener noreferrer">
+                    <img src={gitIcon} alt="git icon" className="contact__icon--git" />
+                </a>
+                <a href="mailto:gerardparedes23@gmail.com" target="_top">
+                    <img src={emailIcon} alt="email icon" className="contact__icon--email" />
+                </a>
+                <a href="https://www.linkedin.com/in/gerardo-paredes-562474168/" target="noopener noreferrer">
+                    <img src={linkedin} alt="linkedIn icon" className="contact__icon--linkedin" />
+                </a>
             </div>
         </div>
     </div>
