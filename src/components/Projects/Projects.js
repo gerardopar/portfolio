7acdb833b34a7b9854707a8a2f3e12a47c300d97
Gerardo Paredes <gerardparedes23@gmail.com>
2020-03-13 11:14:45 -0700

    new project added to portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,24 +13,22 @@ import filmania from '../../assets/img/filmania.png';
 import digiNews from '../../assets/img/diginews.png';
 import pixture from '../../assets/img/pixture-min.png';
 import mayenApparel from '../../assets/img/mayen-apparel.com-min.png';
+import calendApp from '../../assets/img/calendapp-min.png';
 
 const projects = props => (
     <div className="projects" id={props.id}>
-        <ProjectL site="http://cloakdm.s3-website-us-west-1.amazonaws.com/" img={cloakdm} title="cloakdm">
-            <h2 className="projects__col--title">&mdash; 01 CLOAKDM</h2>
+        <ProjectL site="https://github.com/hatchways/team-matcha" img={calendApp} title="calendApp">
+            <h2 className="projects__col--title">&mdash; 01 CALENDAPP</h2>
             <p className="projects__col--text">
-            A private messaging web application.
+            An appointment scheduler application 
             <br />
-            Built using HTML5, SCSS, REACT.JS,
+            powered by Google Calendar API, 
             <br />
-            NODE.JS, EXPRESS, SOCKET.IO
-            <br /> 
-            MONGOOSE & MATERIALIZE CSS.
+            Python (Flask), Javascript (React),
             <br />
-            <a href="http://cloakdm.s3-website-us-west-1.amazonaws.com/" target="noopener noreferrer">
-                <img src={web} alt="world icon" className="projects__col--icon" />
-            </a>
-            <a href="https://github.com/gerardopar/cloakDM-frontEnd-code" target="noopener noreferrer">
+            & MaterialUI 
+            <br />
+            <a href="https://github.com/hatchways/team-matcha" target="noopener noreferrer">
                 <img src={github} alt="github icon" className="projects__col--icon" />
             </a>
             </p>
@@ -75,23 +73,21 @@ const projects = props => (
             </a>
             </p>
         </ProjectL>
-        <ProjectR site="http://digitalnews.s3-website-us-west-1.amazonaws.com/" img={digiNews} title="digiNews">
-            <h2 className="projects__col--title">&mdash; 04 DIGI NEWS</h2>
+        <ProjectR site="http://cloakdm.s3-website-us-west-1.amazonaws.com/" img={cloakdm} title="cloakdm">
+            <h2 className="projects__col--title">&mdash; 04 CLOAKDM</h2>
             <p className="projects__col--text">
-            A real-time news feed
+            A private messaging web application.
             <br />
-            web application.
+            Built using HTML5, SCSS, REACT.JS,
             <br />
-            Built using HTML5, SCSS
+            NODE.JS, EXPRESS, SOCKET.IO
+            <br /> 
+            MONGOOSE & MATERIALIZE CSS.
             <br />
-            REACT.JS, REDUX 
-            <br />
-            & MATERIALIZE CSS.
-            <br />
-            <a href="http://digitalnews.s3-website-us-west-1.amazonaws.com/" target="noopener noreferrer">
+            <a href="http://cloakdm.s3-website-us-west-1.amazonaws.com/" target="noopener noreferrer">
                 <img src={web} alt="world icon" className="projects__col--icon" />
             </a>
-            <a href="https://github.com/gerardopar/diginews" target="noopener noreferrer">
+            <a href="https://github.com/gerardopar/cloakDM-frontEnd-code" target="noopener noreferrer">
                 <img src={github} alt="github icon" className="projects__col--icon" />
             </a>
             </p>
@@ -129,6 +125,27 @@ const projects = props => (
             </a>
             </p>
         </ProjectR>
+        <ProjectL site="http://digitalnews.s3-website-us-west-1.amazonaws.com/" img={digiNews} title="digiNews">
+            <h2 className="projects__col--title">&mdash; 07 DIGI NEWS</h2>
+            <p className="projects__col--text">
+            A real-time news feed
+            <br />
+            web application.
+            <br />
+            Built using HTML5, SCSS
+            <br />
+            REACT.JS, REDUX 
+            <br />
+            & MATERIALIZE CSS.
+            <br />
+            <a href="http://digitalnews.s3-website-us-west-1.amazonaws.com/" target="noopener noreferrer">
+                <img src={web} alt="world icon" className="projects__col--icon" />
+            </a>
+            <a href="https://github.com/gerardopar/diginews" target="noopener noreferrer">
+                <img src={github} alt="github icon" className="projects__col--icon" />
+            </a>
+            </p>
+        </ProjectL>
     </div>
 );
 
