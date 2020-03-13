04e411efbb0189b14a00af15bc9b47d1bdd88e37
Gerardo Paredes <gerardparedes23@gmail.com>
2020-03-13 11:19:55 -0700

    added dynamic year to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const footer = () => (
-    <div className="footer">
-        <p className="footer__text">Copyright&copy; 2019. GERARDO PAREDES All Rights Reserved</p>
-    </div>
-);
+const footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+        <div className="footer">
+            <p className="footer__text">
+                Copyright&copy;
+                {' '}
+                {year}
+                {' '}
+                GERARDO PAREDES All Rights Reserved
+            </p>
+        </div>
+    );
+};
 
 export default footer;
